d71aa03e4f7ce368af2d637858c72c9d51f90ab2
Mr. Sableye <mister.sableye@gmail.com>
2021-08-26 19:42:11 -0500

    Adds banner advertising one of my other projects

--- src/component/App/Header/index.tsx
+++ src/component/App/Header/index.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import './index.css';
 
 const Header = () => (
-  <div className="walker-container">
-    <div className="walker archen" />
-    <div className="walker sableye" />
-    <div className="walker totodile" />
+  <div>
+    <div className="walker-container">
+      <div className="walker archen" />
+      <div className="walker sableye" />
+      <div className="walker totodile" />
+    </div>
+    <div className="passive-aggressive-message">
+      <div>
+        <span>If you enjoy this site, check out </span>
+        <a href="https://clover.weedl.es">Clovermon Showdown</a>
+        <span>: a competitive Pokémon Showdown server featuring fakemons from the popular </span>
+        <a href="https://poclo.net/">Pokémon Clover</a>
+        <span> FireRed ROM hack!</span>
+      </div>
+    </div>
   </div>
 );
 
