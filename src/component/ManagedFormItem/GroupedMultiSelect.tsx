366ca43531fb009aac687babf48806d5f78c6619
Mr. Sableye <mister.sableye@gmail.com>
2021-09-05 02:51:15 -0500

    Adds group selection

--- src/component/ManagedFormItem/GroupedMultiSelect.tsx
+++ src/component/ManagedFormItem/GroupedMultiSelect.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import { Select } from 'antd';
-import { BaseFields, GroupedMultiSelectFields } from './types';
+import React, { useState } from 'react';
+import {
+  Checkbox,
+  Select,
+} from 'antd';
+import {
+  BaseFields,
+  GroupedMultiSelectFields,
+  OptionWithLabel,
+} from './types';
 
 type GroupedMultiSelectProps = BaseFields & GroupedMultiSelectFields;
 
@@ -10,28 +17,64 @@ const GroupedMultiSelect = ({
   enableable,
   selectedOptions,
   setSelectedOptions,
-}: GroupedMultiSelectProps) => (
-  <Select
-    mode="multiple"
-    disabled={enableable ? !enableable.enabled : false}
-    allowClear
-    value={selectedOptions}
-    onChange={setSelectedOptions}
-    placeholder={prompt || ''}
-  >
-    {Object.entries(groupedOptions).map(([groupName, options]) => (
-      <Select.OptGroup label={groupName}>
-        {options.map((option) => (
-          <Select.Option
-            key={option.value}
-            value={option.value}
-          >
-            {option.label || option.value}
-          </Select.Option>
-        ))}
-      </Select.OptGroup>
-    ))}
-  </Select>
-);
+}: GroupedMultiSelectProps) => {
+  const [selectedGroups, setSelectedGroups] = useState<Record<string, boolean>>({});
+
+  const toggleGroup = (
+    options: OptionWithLabel[],
+    isChecked: boolean,
+  ) => {
+    const rawOptions = options.map((option) => option.value);
+    if (isChecked) {
+      setSelectedOptions([
+        ...selectedOptions.filter((previousOption) => !rawOptions.includes(previousOption)),
+        ...rawOptions,
+      ]);
+    } else {
+      setSelectedOptions([
+        ...selectedOptions.filter((previousOption) => !rawOptions.includes(previousOption)),
+      ]);
+    }
+  };
+
+  return (
+    <Select
+      mode="multiple"
+      disabled={enableable ? !enableable.enabled : false}
+      allowClear
+      value={selectedOptions}
+      onChange={setSelectedOptions}
+      placeholder={prompt || ''}
+    >
+      {Object.entries(groupedOptions).map(([groupName, options]) => (
+        <Select.OptGroup
+          label={(
+            <Checkbox
+              checked={selectedGroups[groupName] || false}
+              onChange={(event) => {
+                setSelectedGroups((previousSelectedGroups) => ({
+                  ...previousSelectedGroups,
+                  [groupName]: event.target.checked,
+                }));
+                toggleGroup(options, event.target.checked);
+              }}
+            >
+              {groupName}
+            </Checkbox>
+          )}
+        >
+          {options.map((option) => (
+            <Select.Option
+              key={option.value}
+              value={option.value}
+            >
+              {option.label || option.value}
+            </Select.Option>
+          ))}
+        </Select.OptGroup>
+      ))}
+    </Select>
+  );
+};
 
 export default GroupedMultiSelect;
