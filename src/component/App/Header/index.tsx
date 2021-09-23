341ae14a4b684f05e3e5fb11cf4f2cff0bab9365
Mr. Sableye <mister.sableye@gmail.com>
2021-09-23 01:07:05 -0500

    Removes header

--- src/component/App/Header/index.tsx
+++ src/component/App/Header/index.tsx
@@ -8,15 +8,6 @@ const Header = () => (
       <div className="walker sableye" />
       <div className="walker totodile" />
     </div>
-    <div className="passive-aggressive-message">
-      <div>
-        <span>If you enjoy this site, check out </span>
-        <a href="https://clover.weedl.es">Clovermon Showdown</a>
-        <span>: a competitive Pokémon Showdown server featuring fakemons from the popular </span>
-        <a href="https://poclo.net/">Pokémon Clover</a>
-        <span> FireRed ROM hack!</span>
-      </div>
-    </div>
   </div>
 );
 
